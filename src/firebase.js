c40174065416ac0866f1d5b00dafb97ed9444d30
gauravsewani <lavishgps@gmail.com>
2022-11-01 19:39:42 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD51SDB4tGFxRifdKISVJzKqkNG3E1YYUE",
+  authDomain: "movie-streaming-303ba.firebaseapp.com",
+  projectId: "movie-streaming-303ba",
+  storageBucket: "movie-streaming-303ba.appspot.com",
+  messagingSenderId: "673894143447",
+  appId: "1:673894143447:web:1e9cadd6331ae4129e6792",
+};
